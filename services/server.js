e199c1248649967edf03c5fe6efe058c0c7667dd
Nate Johnson <njohnson@cars.com>
2016-02-23 08:20:39 -0600

    Add bluebird

--- services/server.js
+++ services/server.js
@@ -6,6 +6,7 @@ const restify = require('restify');
 const url = require('url');
 const _ = require('lodash');
 const moment = require('moment');
+const Promise = require("bluebird");
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectID;
 require('moment-duration-format');
@@ -28,23 +29,31 @@ server.use(restify.fullResponse());
 
 // GET
 server.get('/hars/:id', function (req, res, next) {
-
-    MongoClient.connect('mongodb://' + config.get('db.host') + '/' + config.get('db.database'), function (err, db) {
-        if (err) throw err;
-
-        db.collection('hars').findOne({"_id": new ObjectId(req.params.id)}, function (err, har) {
-            if (err) throw err;
-
-            //let data = {};
-
-            res.send(har);
-
-            db.close();
-
-            return next();
-        });
+    findHAR(req.params.id).then(function(har) {
+        res.send(har)
+        return next();
     });
 });
+
+
+server.get('/entry-report/:id', function (req, res, next) {
+    res.send({});
+    return next();
+});
+
+function findHAR(id) {
+    return new Promise(function (resolve, reject) {
+        MongoClient.connect('mongodb://' + config.get('db.host') + '/' + config.get('db.database'), function (err, db) {
+            if (err) throw err;
+
+            db.collection('hars').findOne({"_id": new ObjectId(id)}, function (err, har) {
+                if (err) throw err;
+                resolve(har);
+                db.close();
+            });
+        });
+    });
+}
 
 // start server
 server.listen(port, function () {

cff51bdd4e91defc213e2475d1ce5413cde22bfd
Nate Johnson <njohnson@cars.com>
2016-02-23 17:50:24 -0600

    Add all hars endpoint

--- services/server.js
+++ services/server.js
@@ -21,38 +21,123 @@ let server = restify.createServer({
 
 let argv = require('minimist')(process.argv.slice(2));
 let port = argv['p'] || config.get('server.port');
+let trusted = config.get('sites.trusted');
 
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 server.use(restify.fullResponse());
 
-// GET
-server.get('/hars/:id', function (req, res, next) {
-    findHAR(req.params.id).then(function(har) {
-        res.send(har)
-        return next();
-    });
+
+server.get('/hars', function (req, res, next) {
+    findHARs()
+        .then(function (data) {
+            res.send(data);
+            return next();
+        })
+        .catch(function (err) {
+            res.send(500, 'Server error');
+            return next();
+        });
 });
 
+server.get('/hars/:id', function (req, res, next) {
+    findHAR(req.params.id)
+        .then(function (har) {
+            res.send(har);
+            return next();
+        })
+        .catch(function (err) {
+            res.send(500, 'Server error');
+            return next();
+        });
+});
 
 server.get('/entry-report/:id', function (req, res, next) {
-    res.send({});
-    return next();
+    findHAR(req.params.id)
+        .then(function (har) {
+            res.send(analyze(har));
+            return next();
+        })
+        .catch(function (err) {
+            res.send(500, 'Server error');
+            return next();
+        });
 });
 
-function findHAR(id) {
+function findHARs() {
     return new Promise(function (resolve, reject) {
         MongoClient.connect('mongodb://' + config.get('db.host') + '/' + config.get('db.database'), function (err, db) {
-            if (err) throw err;
+            if (err) reject(err);
 
-            db.collection('hars').findOne({"_id": new ObjectId(id)}, function (err, har) {
-                if (err) throw err;
-                resolve(har);
+            db.collection('hars').find({}, {
+                "log.pages.title": 1,
+                "log.pages.id": 1,
+                "log.pages.startedDateTime": 1
+            }).toArray(function (err, docs) {
+                if (err) reject(err);
+
+                resolve(docs);
+
                 db.close();
             });
         });
     });
+}
+
+function findHAR(id) {
+    return new Promise(function (resolve, reject) {
+        MongoClient.connect('mongodb://' + config.get('db.host') + '/' + config.get('db.database'), function (err, db) {
+            if (err) reject(err);
+
+            db.collection('hars').findOne({"_id": new ObjectId(id)}, function (err, har) {
+                if (err) reject(err);
+
+                resolve(har);
+
+                db.close();
+            });
+        });
+    });
+}
+
+function analyze(har) {
+    let retval = {};
+    let data = {};
+
+    retval['url'] = har.log.pages[0].id;
+    retval['title'] = har.log.pages[0].title;
+    retval['date'] = har.log.pages[0].startedDateTime;
+
+    // index results
+    har.log.entries.forEach(function (entry) {
+        let hostname = url.parse(entry.request.url).hostname;
+        let item = data[hostname];
+        let cnt = (item && item.hasOwnProperty('count')) ? parseInt(item.count) : 0;
+        let time = (item && item.hasOwnProperty('time')) ? parseFloat(item.time) : 0;
+
+        data[hostname] = {
+            count: cnt + 1,
+            time: time + entry.time
+        };
+    });
+
+    // assemble results
+    let results = [];
+    for (let hostname in data) {
+        let obj = data[hostname];
+
+        results.push({
+            y: obj.time,
+            indexLabel: hostname + ' #percent%',
+            legendText: hostname,
+            duration: moment.duration(obj.time).format("s[s], S[ms]")
+        });
+    }
+
+    retval['dataPoints'] = _.orderBy(results, ['y'], ['desc']);
+
+    return retval;
 }
 
 // start server
--- webapp/app.js
+++ webapp/app.js
@@ -54,11 +54,13 @@ var routes = [{
     method: 'GET',
     path: '/',
     handler: function (request, reply) {
-        var dataPoints = [
-            {y: 65899660, indexLabel: "Trusted (#percent%)", legendText: "Trusted"},
-            {y: 60929152, indexLabel: "3rd Party (#percent%)", legendText: "3rd Party"},
-            {y: 2175850, indexLabel: "Others #percent%", legendText: "Others"}
-        ];
+        //var dataPoints = [
+        //    {y: 65899660, indexLabel: "Trusted (#percent%)", legendText: "Trusted"},
+        //    {y: 60929152, indexLabel: "3rd Party (#percent%)", legendText: "3rd Party"},
+        //    {y: 2175850, indexLabel: "Others #percent%", legendText: "Others"}
+        //];
+
+        var dataPoints = [{"y":3398,"indexLabel":"www.cstatic-graphics.com #percent%","legendText":"www.cstatic-graphics.com"},{"y":2801,"indexLabel":"www.cstatic-images.com #percent%","legendText":"www.cstatic-images.com"},{"y":2227,"indexLabel":"www.collserve.com #percent%","legendText":"www.collserve.com"},{"y":1424,"indexLabel":"www.cars.com #percent%","legendText":"www.cars.com"},{"y":1291,"indexLabel":"securepubads.g.doubleclick.net #percent%","legendText":"securepubads.g.doubleclick.net"},{"y":1078,"indexLabel":"s.thebrighttag.com #percent%","legendText":"s.thebrighttag.com"},{"y":1016,"indexLabel":"fonts.gstatic.com #percent%","legendText":"fonts.gstatic.com"},{"y":814,"indexLabel":"tpc.googlesyndication.com #percent%","legendText":"tpc.googlesyndication.com"},{"y":220,"indexLabel":"vcu.collserve.com #percent%","legendText":"vcu.collserve.com"},{"y":210,"indexLabel":"carscomconsumer.collect.igodigital.com #percent%","legendText":"carscomconsumer.collect.igodigital.com"},{"y":182,"indexLabel":"www.googletagservices.com #percent%","legendText":"www.googletagservices.com"},{"y":177,"indexLabel":"us-sonar.sociomantic.com #percent%","legendText":"us-sonar.sociomantic.com"},{"y":107,"indexLabel":"fonts.googleapis.com #percent%","legendText":"fonts.googleapis.com"},{"y":97,"indexLabel":"ssl.google-analytics.com #percent%","legendText":"ssl.google-analytics.com"},{"y":86,"indexLabel":"sb.scorecardresearch.com #percent%","legendText":"sb.scorecardresearch.com"},{"y":80,"indexLabel":"pagead2.googlesyndication.com #percent%","legendText":"pagead2.googlesyndication.com"},{"y":75,"indexLabel":"s.btstatic.com #percent%","legendText":"s.btstatic.com"},{"y":74,"indexLabel":"z.moatads.com #percent%","legendText":"z.moatads.com"},{"y":66,"indexLabel":"assets.adobedtm.com #percent%","legendText":"assets.adobedtm.com"},{"y":65,"indexLabel":"www.google-analytics.com #percent%","legendText":"www.google-analytics.com"},{"y":54,"indexLabel":"s0.2mdn.net #percent%","legendText":"s0.2mdn.net"},{"y":47,"indexLabel":"bat.bing.com #percent%","legendText":"bat.bing.com"},{"y":32,"indexLabel":"partner.googleadservices.com #percent%","legendText":"partner.googleadservices.com"}];
 
         var data = {
             title: 'FE Aware',
